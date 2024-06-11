6d86651422722eba172b75f7c84baff110836956
AshyLarryM <LarryM093@gmail.com>
2024-06-11 10:56:28 -0400

    additional app layouts + tailwind colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="">
-      Test
-    </main>
+    <>
+      <div className="min-h-full bg-black text-white">
+        Test
+      </div>
+    </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,17 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <header className='flex items-center justify-center h-[70px] w-full z-10 bg-black'>
+      <div className="relative flex justify-between items-center max-w-screen-xl w-full px-5">
+        <Link href="/">
+          <div className="flex items-center cursor-pointer">
+            <img src="https://picsum.photos/seed/picsum/40" alt="Nerve Lab Logo" className="h-10" />
+            <span className="ml-4 text-white font-medium text-xl"><span className='text-bright-green 2xl'>Nerve</span>Lab</span>
+          </div>
+        </Link>
+      </div>
+    </header>
   )
 }
