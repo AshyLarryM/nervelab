3514c892c85bce325a191f38944e7f8fc7775468
AshyLarryM <LarryM093@gmail.com>
2024-06-22 14:52:29 -0400

    React Query init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NextAuthProvider from "@/components/NextAuthProvider";
+import { ReactQueryProvider } from "@/components/ReactQueryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,16 +16,18 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full">
-      <body className={`h-full bg-dark-futuristic ${inter.className}`}>
-        <NextAuthProvider>
-          <div className="flex flex-col h-full ">
-            <main className="flex-1">
-              {children}
-            </main>
-          </div>
-        </NextAuthProvider>
-      </body>
-    </html>
+    <ReactQueryProvider>
+      <html lang="en" className="h-full">
+        <body className={`h-full bg-dark-futuristic ${inter.className}`}>
+          <NextAuthProvider>
+            <div className="flex flex-col h-full ">
+              <main className="flex-1">
+                {children}
+              </main>
+            </div>
+          </NextAuthProvider>
+        </body>
+      </html>
+    </ReactQueryProvider>
   );
 }
