4baaa6c78940430ebd23308dcd1d80db95a7cda6
AshyLarryM <LarryM093@gmail.com>
2024-06-27 14:28:26 -0400

    test for ios mobile devices

--- src/components/LoadingHome.tsx
+++ src/components/LoadingHome.tsx
@@ -1,11 +1,11 @@
-'use client'
+
 import React from 'react'
 import Image from 'next/image'
 
 export default function LoadingHome() {
   return (
     <div className="flex justify-center items-center h-screen bg-black">
-      <Image src='/assets/logo/nervelablogo.png?version=1' alt="nervelab-logo" className="" height='200' width='200' />
+      <Image src='/assets/logo/nervelablogo.png?version=1' alt="nervelab-logo" className=" image-glow" height='200' width='200' />
     </div>
   )
 }
