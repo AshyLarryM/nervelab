656dd096e0375a6c1595b88fa6af16877ac52318
AshyLarryM <LarryM093@gmail.com>
2024-06-27 13:59:20 -0400

    <Image/> component test

--- src/components/LoadingHome.tsx
+++ src/components/LoadingHome.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function LoadingHome() {
   return (
     <div className="flex justify-center items-center h-screen bg-black">
-      <img src='/assets/logo/nervelab-logo.png?version=1' alt="Company Logo" className="w-48 image-glow" />
+      <Image src='/assets/logo/nervelab-logo.png?version=1' alt="Company Logo" className="w-48 image-glow" />
     </div>
   )
 }
