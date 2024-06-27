2c1f556d48d7a30f17468ff4b9e0cd8e7a5a755e
AshyLarryM <LarryM093@gmail.com>
2024-06-27 14:02:37 -0400

    Image Component height/width

--- src/components/LoadingHome.tsx
+++ src/components/LoadingHome.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function LoadingHome() {
   return (
     <div className="flex justify-center items-center h-screen bg-black">
-      <Image src='/assets/logo/nervelab-logo.png?version=1' alt="Company Logo" className="w-48 image-glow" />
+      <Image src='/assets/logo/nervelab-logo.png?version=1' alt="Company Logo" className=" image-glow" height='200' width='200' />
     </div>
   )
 }
