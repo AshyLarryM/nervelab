9bddce248d1cc30771c43c4dbed9474f72bf4ca5
AshyLarryM <LarryM093@gmail.com>
2024-07-16 14:03:45 -0700

    Interfont for AdminPageFrame

--- src/components/admin/AdminPageFrame.tsx
+++ src/components/admin/AdminPageFrame.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AdminNavbar } from '../admin/AdminNavbar';
 import { AdminSidebar } from '../admin/AdminSidebar';
+import { Inter } from 'next/font/google';
 
 interface AdminPageFrameProps {
   children: React.ReactNode;
@@ -10,13 +11,16 @@ interface AdminPageFrameProps {
   showAdminSidebar?: boolean;
 }
 
+
+const inter = Inter({ subsets: ["latin"] });
+
 export default function AdminPageFrame({ children, showNavbar = true, showFooter = true, showParticles = false, showAdminSidebar = true }: AdminPageFrameProps) {
   return (
     <div className="flex flex-col h-screen">
       {showNavbar && <AdminNavbar />}
       <div className="flex flex-1 overflow-hidden">
         {showAdminSidebar && <AdminSidebar />}
-        <main className="flex-1 flex flex-col overflow-y-auto p-4">
+        <main className={`flex-1 flex flex-col overflow-y-auto p-4 ${inter.className}`}>
           {children}
         </main>
       </div>
