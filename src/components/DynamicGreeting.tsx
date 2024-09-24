a58d168ffefaacb0787100860aea47bf202045f6
AshyLarryM <LarryM093@gmail.com>
2024-09-23 20:04:58 -0700

    removed toUpperCase function in DynamicGreeting.

--- src/components/DynamicGreeting.tsx
+++ src/components/DynamicGreeting.tsx
@@ -3,18 +3,10 @@ interface DynamicGreetingProps {
 }
 
 export function DynamicGreeting({ username }: DynamicGreetingProps) {
-
-    function capitalizeFirstLetter(username: string) {
-        if (!username) {
-            return "";
-        }
-        return username.charAt(0).toUpperCase() + username.slice(1);
-    }
-
     return (
         <div className="mt-6 mb-12 rounded-lg gradient-greeting px-4 py-8 md:mt-2 md:mb-16 md:px-16 md:py-12">
             <div className="text-center">
-                <h2 className="text-3xl md:text-5xl text-white mb-2 md:mb-4 text-glow-cyan">Hello, {capitalizeFirstLetter(username)}</h2>
+                <h2 className="text-3xl md:text-5xl text-white mb-2 md:mb-4 text-glow-cyan">Hello, {username}</h2>
                 <p className="text-white font-medium text-lg md:text-2xl text-header-glow">Please Login to your account</p>
             </div>
         </div>
