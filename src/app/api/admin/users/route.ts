c5731edcb8449cd3ca663e3ba3acd08e13fe25a3
AshyLarryM <LarryM093@gmail.com>
2024-10-17 15:24:07 -0700

    "force-dynamic" in /api/admin/users

--- src/app/api/admin/users/route.ts
+++ src/app/api/admin/users/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { UpdateUserSchema, UserSchema } from '@/lib/utils/schema';
 import { prisma } from '@/lib/utils/prismadb';
 
+export const dynamic = 'force-dynamic';
 
 export async function GET(req: NextRequest) {
   try {
