0a537874d2c280809945457727750d0ebf3102b6
AshyLarryM <LarryM093@gmail.com>
2024-10-17 09:06:36 -0700

    dynamic greeting red text

--- src/components/DynamicGreeting.tsx
+++ src/components/DynamicGreeting.tsx
@@ -8,7 +8,7 @@ export function DynamicGreeting({ username }: DynamicGreetingProps) {
         <div className="mt-6 mb-12 max-w-[300px] md:max-w-[500px] lg:max-w-none rounded-lg gradient-greeting px-4 py-8 md:mt-2 md:mb-16 md:px-16 md:py-12">
             <div className="text-center">
                 <h2 className="text-3xl md:text-5xl text-white mb-3 md:mb-4 text-glow-cyan">Hello, {username}</h2>
-                <p className="text-white font-medium text-md md:text-3xl text-glow my-2 md:my-4">This is Vector.</p>
+                <p className="text-red-100 font-medium text-lg md:text-3xl text-glow-red my-2 md:my-4">I am VECTOR.</p>
                 <p className="text-white font-medium text-md md:text-2xl mb-3 text-glow-cyan">Sign in with USERNAME: <span className="text-glow">OrdersAndChaos@nervelabs.ai</span></p>
                 <p className="text-white font-medium text-md md:text-2xl text-glow-cyan">Password: <span className="text-glow">NoPickles123!</span></p>
             </div>
