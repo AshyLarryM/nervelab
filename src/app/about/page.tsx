af0d7de7430440a14df6a3e4168572de6a8b21e2
AshyLarryM <LarryM093@gmail.com>
2024-10-17 08:41:16 -0700

    Scenario1 Updated Text, About Page updated images and descriptions.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,19 +10,19 @@ interface AboutProps {
 
 const aboutData: AboutProps[] = [
   {
-    title: 'About 1',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis suscipit purus. Aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis suscipit purus. Aliquam erat volutpat',
-    image: 'https://picsum.photos/seed/picsum/200',
+    title: 'Embrace the Digital Frontier',
+    description: "We're redefining the boundaries between reality and the digital realm. VectorBall isn't just a game—it's an odyssey through consciousness and code. Join Vector, our advanced AI, as you traverse worlds inspired by ancient civilizations, solve intricate puzzles, and explore the depths of collective human wisdom. Are you ready to step into the unknown and shape the future of interactive experiences?",
+    image: '/assets/anubis.jpg',
   },
   {
-    title: 'About 2',
-    description: 'Second description goes here. Vivamus lacinia odio vitae vestibulum vestibulum. Second description goes here. Vivamus lacinia odio vitae vestibulum vestibulum',
-    image: 'https://picsum.photos/seed/picsum/201',
+    title: 'The Symphony of Innovation',
+    description: "Innovation at NerveLabs resonates beyond visuals—it's an experience that engages all senses. Music is the heartbeat of VectorBall, crafted to immerse you fully in the adventure. Our synthesizer technology bridges the gap between player and sound, creating unique soundscapes. With over 200 unique songs, each note and harmony is intertwined with your journey, echoing the themes of exploration and transformation that define our creations.",
+    image: '/assets/cs80.jpg',
   },
   {
-    title: 'About 3',
-    description: 'Third description goes here. Cras vel erat nec massa ultricies feugiat. Second description goes here. Vivamus lacinia odio vitae vestibulum vestibulum Second description goes here. Vivamus lacinia odio vitae vestibulum vestibulum',
-    image: 'https://picsum.photos/seed/picsum/202',
+    title: 'Unite, Compete, Unveil Truths',
+    description: "At NerveLabs, we explore the depths of human connection and competition. VectorBall offers a unique experience where you and your friends can delve into expansive open worlds inspired by ancient civilizations. Collaborate to overcome intricate challenges, unlock hidden secrets, and influence a narrative that evolves with every choice you make. But alliances are not always steadfast. Engage in dynamic PVP battles that will test your skills and the bonds of friendship. The co-op campaign is designed to challenge not just your prowess but your honesty, occasionally pitting you against your companions in unexpected ways. As you journey with Vector, you'll discover that the true nature of humanity lies in the balance between cooperation and competition. Are you ready to face not only the game's mysteries but also the reflections of yourselves?",
+    image: '/assets/fractal.jpg',
   }
 ];
 
@@ -40,7 +40,7 @@ function About({ title, description, image, reverse }: AboutProps) {
           </div>
         </div>
         <div className="flex-1 flex items-center justify-center bg-transparent/40 p-8">
-          <img src={image} alt="Descriptive Alt Text" className="w-96 h-96 object-cover rounded-lg hero-gradient" />
+          <img src={image} alt="Descriptive Alt Text" className="w-auto h-auto object-cover rounded-lg hero-gradient" />
         </div>
       </div>
     </div>
--- src/components/DynamicGreeting.tsx
+++ src/components/DynamicGreeting.tsx
@@ -8,8 +8,9 @@ export function DynamicGreeting({ username }: DynamicGreetingProps) {
         <div className="mt-6 mb-12 max-w-[300px] md:max-w-[500px] lg:max-w-none rounded-lg gradient-greeting px-4 py-8 md:mt-2 md:mb-16 md:px-16 md:py-12">
             <div className="text-center">
                 <h2 className="text-3xl md:text-5xl text-white mb-3 md:mb-4 text-glow-cyan">Hello, {username}</h2>
-                <p className="text-white font-medium text-md md:text-2xl mb-3 text-glow-cyan">Sign in with USERNAME: <span className="text-glow">JohnDoe@nervelabs.com</span></p>
-                <p className="text-white font-medium text-md md:text-2xl text-glow-cyan">Password: <span className="text-glow">J8!x4F@2LbZ#9kPm</span></p>
+                <p className="text-white font-medium text-md md:text-3xl text-glow my-2 md:my-4">This is Vector.</p>
+                <p className="text-white font-medium text-md md:text-2xl mb-3 text-glow-cyan">Sign in with USERNAME: <span className="text-glow">OrdersAndChaos@nervelabs.ai</span></p>
+                <p className="text-white font-medium text-md md:text-2xl text-glow-cyan">Password: <span className="text-glow">NoPickles123!</span></p>
             </div>
         </div>
     )
